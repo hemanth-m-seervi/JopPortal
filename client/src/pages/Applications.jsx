bf5ffbc13601760001092678a25da639dd7348dd
hemanth-m-seervi <hemanthmsirvi@gmail.com>
2025-08-30 12:54:38 +0530

    final

--- client/src/pages/Applications.jsx
+++ client/src/pages/Applications.jsx
@@ -70,7 +70,11 @@ const Applications = () => {
             <button onClick={updateResume} className='bg-green-100 border border-green-400 rounded-lg px-4 py-2 cursor-pointer'>Save</button>
             </>
             :<div className='flex gap-2'>
-              <a target='_blank' href={userData.resume} className='bg-blue-100 text-blue-600 px-4 py-2 rounded-lg ' >Resume</a>
+              {userData && userData.resume ? (
+                <a target='_blank' href={userData.resume + '?fl_attachment=false'} className='bg-blue-100 text-blue-600 px-4 py-2 rounded-lg'>Resume</a>
+              ) : (
+                <span className='bg-blue-100 text-blue-600 px-4 py-2 rounded-lg'>No resume uploaded</span>
+              )}
               <button onClick={()=> setIsEdit(true)} className='text-gray-500 border border-gray-300 rounded-lg px-4 py-2 cursor-pointer'>
                 Edit
               </button>
